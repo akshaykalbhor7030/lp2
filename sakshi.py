18e1f691c924ef98aee8874bce901762461eb176
akshaykalbhor7030 <156069715+akshaykalbhor7030@users.noreply.github.com>
2024-05-10 12:06:37 +0530

    Update sakshi.py

--- sakshi.py
+++ sakshi.py
@@ -2,7 +2,7 @@
 num = float(input("Enter a number: "))
 
 # Checking if the number is positive, negative, or zero
-if num % 2:
+if num % 2==0:
     print("The number is positive.")
 else :
     print("The number is negative.")
